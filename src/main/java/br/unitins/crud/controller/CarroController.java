d2da6df12d0fc289fe16a03ed5cc2c5f58e2632c
Lucas José <lucasjs088@gmail.com>
2022-02-26 11:18:13 -0300

    Crud completo

--- src/main/java/br/unitins/crud/controller/CarroController.java
+++ src/main/java/br/unitins/crud/controller/CarroController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import br.unitins.crud.application.Util;
 import br.unitins.crud.model.Carro;
 
 @Named
@@ -16,46 +17,57 @@ public class CarroController implements Serializable {
 	private static final long serialVersionUID = 6622012672983485067L;
 	private Carro carro;
 	private List<Carro> listaCarros;
-	
-	
-	
+	private int cont =1;
+
 	public Carro getCarro() {
-		if(carro == null) {
+		if (carro == null) {
 			carro = new Carro();
 		}
 		return carro;
 	}
+
 	public void setCarro(Carro carro) {
 		this.carro = carro;
 	}
-	
+
 	public List<Carro> getListaCarros() {
-		if(listaCarros == null) {
-			listaCarros= new ArrayList<Carro>();
+		if (listaCarros == null) {
+			listaCarros = new ArrayList<Carro>();
 		}
 		return listaCarros;
 	}
+
 	public void setListaCarros(List<Carro> listaCarros) {
 		this.listaCarros = listaCarros;
 	}
-	
-	
+
 	public void cadastrar() {
-		
+
+		getCarro().setId(cont++);
 		getListaCarros().add(carro);
 		limpar();
+		
+		Util.addMsg("Cadastro realizado com sucesso!");
 	}
 	
+	public void alterar() {
+		
+		int index = listaCarros.indexOf(getCarro());
+		listaCarros.set(index, getCarro());
+		
+	}
+
+	public void remover(Carro carro) {
+		listaCarros.remove(carro);
+	}
+	public void editar(Carro carro) {
+	
+		setCarro(carro.getClone());
+	}
 	public void limpar() {
-		
-		carro =null;
-		
-//		carro.setNome(null);
-//		carro.setMarca(null);
-//		carro.setCor(null);
-//		carro.setPlaca(null);
-//		carro.setPotencia(null);
+
+		carro = null;
+
 	}
-	
-	
+
 }
--- src/main/java/br/unitins/crud/model/Carro.java
+++ src/main/java/br/unitins/crud/model/Carro.java
@@ -1,7 +1,10 @@
 package br.unitins.crud.model;
 
-public class Carro {
+import java.util.Objects;
 
+public class Carro implements Cloneable{
+
+	private Integer id;
 	private String nome;
 	private String marca;
 	private String cor;
@@ -9,7 +12,26 @@ public class Carro {
 	private String potencia;
 	
 	
+	public Carro getClone() {
+		
+		try {
+			return (Carro)super.clone();
+			
+		} catch (CloneNotSupportedException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+}
+
 	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -40,6 +62,26 @@ public class Carro {
 	public void setPotencia(String potencia) {
 		this.potencia = potencia;
 	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carro other = (Carro) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 	
